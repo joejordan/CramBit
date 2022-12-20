a079d766b3f18614c02c7d1c3c1b441e2be6363a
Joe Jordan <dev@ignitesoft.com>
2022-12-20 17:54:53 -0500

    style: fix prettier formatting issues

--- script/CramBit.s.sol
+++ script/CramBit.s.sol
@@ -6,7 +6,6 @@ import { CramBit } from "src/CramBit.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract CramBitScript is Script {
-
     function run() public {
         vm.startBroadcast();
         // stub
